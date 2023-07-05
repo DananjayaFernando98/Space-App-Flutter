849f03b76e1da32ad9fcb51d862799b9e8a34032
DananjayaFernando98 <danuka19980@gmail.com>
2023-07-05 22:33:19 +0530

    create body section

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,53 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Space Details",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/space1.png",
+                    height: 300,
+                    scale: 2,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  color: Colors.redAccent,
+                  child: const Text(
+                    "Space Details",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
