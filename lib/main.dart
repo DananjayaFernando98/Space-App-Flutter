bc8d34341e01bb30e9741e4ada2075bd99d757be
DananjayaFernando98 <danuka19980@gmail.com>
2023-07-05 21:06:53 +0530

    Create App Bar

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Space App",
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          shadowColor: Colors.yellow,
+          title: const Text(
+            "BLACK HOLE",
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 30,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
