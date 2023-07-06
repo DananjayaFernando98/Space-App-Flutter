adc73f1b18ea9d5393b8990af0588e9ae2930dd8
DananjayaFernando98 <danuka19980@gmail.com>
2023-07-06 12:10:13 +0530

    finally create simple flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -34,42 +34,44 @@ class MyApp extends StatelessWidget {
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Space Details",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Text(
+                "Space Details",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
                 ),
-                const SizedBox(
-                  height: 50,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: Image.asset(
+                  "assets/space1.png",
+                  height: 300,
+                  scale: 2,
                 ),
-                Center(
-                  child: Image.asset(
-                    "assets/space1.png",
-                    height: 300,
-                    scale: 2,
-                  ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
                 ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Center(
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              //button
+              GestureDetector(
+                onTap: () {},
+                child: Center(
                   child: Container(
                     padding: const EdgeInsets.all(15.0),
                     decoration: BoxDecoration(
@@ -82,8 +84,132 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              //SecondScreen
+              Center(
+                child: Image.asset(
+                  "assets/space2.png",
+                  height: 300,
+                  scale: 2,
+                ),
+              ),
+              const Text(
+                "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.pink,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.red,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.pink,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //ThirsSCreen
+              Center(
+                child: Image.asset(
+                  "assets/space3.png",
+                  height: 300,
+                  scale: 2,
+                ),
+              ),
+              const Text(
+                "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.pink,
+                    ),
+                    child: const Text(
+                      "Space Details",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              //footer
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 2,
+                width: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white30,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "BLACK HOLE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "that error happens because your anti-virus moved the adb.exe to the vault and block it, so you need to give that file an exception that it is not a virus, if that didn't fix it you may need to uninstall the SDK tools and reinstall it again to work",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ]),
           ),
         ),
       ),
